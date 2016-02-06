2d812f8731ab6e7f1c9df983fd057196659124fe
lazarev <diedsmiling@gmail.com>
2016-02-06 19:53:09 +0200

    Started options validation

--- tests/specs/cupit.spec.js
+++ tests/specs/cupit.spec.js
@@ -1,7 +1,36 @@
 'use strict';
 var cupit = require('../../index');
-describe('cupit', () => {
-  it('should check context instance', function() {
-    cupit();
+describe('Cupit ', () => {
+
+  it('should call init function', function() {
+    sinon.spy(cupit.prototype, 'init');
+    cupit({
+      matchesPath: 'data/match-list.js'
+    });
+    expect(cupit.prototype.init).to.be.called;
+    cupit.prototype.init.restore();
   });
+
+  describe('while initializing ', function() {
+
+    it('Should validate options', function() {
+      sinon.spy(cupit.prototype, '_validateOptions');
+      cupit({
+        matchesPath: 'data/match-list.js'
+      });
+      expect(cupit.prototype._validateOptions).to.be.called;
+    });
+
+  });
+
+  describe('while validating options', function() {
+
+    it('it should throw an error if no matches given', function() {
+      expect(function() {
+        cupit({});
+      }).to.throw('An empty list of matches was set');
+    });
+
+  });
+
 });
